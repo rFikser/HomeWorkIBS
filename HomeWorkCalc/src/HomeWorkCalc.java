f48c9939a1c8e8482b38bff0550eef43091f2998
rFikser <john321cheba@gmail.com>
2022-04-29 23:21:43 +0300

    HomeWorkCalc

---
+++ HomeWorkCalc/src/HomeWorkCalc.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class HomeWorkCalc {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double x = scan.nextDouble();
+        double y = scan.nextDouble();
+        String operation = scan.next();
+        switch (operation) {
+            case "+":
+                System.out.println(x + y);
+                break;
+            case "-":
+                System.out.println(x - y);
+                break;
+            case "*":
+                System.out.println(x * y);
+                break;
+            case "/":
+                if (y == 0) {
+                    System.out.println("Делить на ноль нельзя!");
+                } else {
+                    System.out.println(x / y);
+                }
+                break;
+        }
+    }
+}
